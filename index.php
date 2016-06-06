dedff62c92c17b82dc977a80c15182e3eaa1b368
davidgamaliel <davidgamaliel.sihombing@gmail.com>
2016-06-06 11:57:49 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,238 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Indonesian Youth 4 SDGs</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet"> 
+    <link href="css/lightbox.css" rel="stylesheet"> 
+	<link href="css/main.css" rel="stylesheet">
+	<link href="css/responsive.css" rel="stylesheet">
+
+    <!--[if lt IE 9]>
+	    <script src="js/html5shiv.js"></script>
+	    <script src="js/respond.min.js"></script>
+    <![endif]-->       
+    <link rel="shortcut icon" href="images/slider/logo-sgd.jpg">
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="images/slider/logo-sgd.jpg">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="images/slider/logo-sgd.jpg">
+    <link rel="apple-touch-icon-precomposed" sizes="72x72" href="images/slider/logo-sgd.jpg">
+    <link rel="apple-touch-icon-precomposed" href="images/slider/logo-sgd.jpg">
+</head><!--/head-->
+
+<body>
+	<header id="header">      
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 overflow">
+                   <div class="social-icons pull-right">
+                   <!--
+                        <ul class="nav nav-pills">
+                            <li><a href=""><i class="fa fa-facebook"></i></a></li>
+                            <li><a href=""><i class="fa fa-twitter"></i></a></li>
+                            <li><a href=""><i class="fa fa-google-plus"></i></a></li>
+                            <li><a href=""><i class="fa fa-dribbble"></i></a></li>
+                            <li><a href=""><i class="fa fa-linkedin"></i></a></li>
+                        </ul>
+                    -->
+                    </div> 
+                </div>
+             </div>
+        </div>
+        <div class="navbar navbar-inverse" role="banner">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <a class="navbar-brand" href="index.html">
+                    	<h1><img src="images/slider/logo-head-blue.jpg" width="400px" alt="logo"></h1>
+                    </a>
+                    
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="active"><a href="index.html" style="font-family:Comfortaa">Home</a></li>
+                        <li class="dropdown"><a style="font-family:Comfortaa">About Us <i class="fa fa-angle-down"></i></a>
+                            <ul role="menu" class="sub-menu">
+                                <li><a href="aboutus-vision.html" style="font-family:Comfortaa">Vision & Mission</a></li>
+                                <li><a href="meet-the-team.html" style="font-family:Comfortaa">Meet The Team</a></li>
+                                <!--
+                                <li><a href="aboutus2.html">About 2</a></li>
+                                <li><a href="service.html">Services</a></li>
+                                <li><a href="pricing.html">Pricing</a></li>
+                                <li><a href="contact.html">Contact us</a></li>
+                                <li><a href="contact2.html">Contact us 2</a></li>
+                                <li><a href="404.html">404 error</a></li>
+                                <li><a href="coming-soon.html">Coming Soon</a></li>
+                                -->
+                            </ul>
+                        </li>                    
+                        <li class="dropdown"><a style="font-family:Comfortaa">Get Involved <i class="fa fa-angle-down"></i></a>
+                            <ul role="menu" class="sub-menu">
+                                <li><a href="what-we-do.html" style="font-family:Comfortaa">Selfie For SDGs</a></li>
+                                <li><a href="online-volunteering-program.html" style="font-family:Comfortaa">Online Volunteering Program</a></li>
+                            </ul>
+                            <!--
+                            <ul role="menu" class="sub-menu">
+                                <li><a href="blog.html">Blog Default</a></li>
+                                <li><a href="blogtwo.html">Timeline Blog</a></li>
+                                <li><a href="blogone.html">2 Columns + Right Sidebar</a></li>
+                                <li><a href="blogthree.html">1 Column + Left Sidebar</a></li>
+                                <li><a href="blogfour.html">Blog Masonary</a></li>
+                                <li><a href="blogdetails.html">Blog Details</a></li>
+                            </ul>
+                            -->
+                        </li>
+                        <li><a href="sgd.html" style="font-family:Comfortaa">SDGs</a>
+                            <!--
+                            <ul role="menu" class="sub-menu">
+                                <li><a href="portfolio.html">Portfolio Default</a></li>
+                                <li><a href="portfoliofour.html">Isotope 3 Columns + Right Sidebar</a></li>
+                                <li><a href="portfolioone.html">3 Columns + Right Sidebar</a></li>
+                                <li><a href="portfoliotwo.html">3 Columns + Left Sidebar</a></li>
+                                <li><a href="portfoliothree.html">2 Columns</a></li>
+                                <li><a href="portfolio-details.html">Portfolio Details</a></li>
+                            </ul>
+                            -->
+                        </li>                         
+                        <li><a href="news.html" style="font-family:Comfortaa">News & Media</a></li>  
+                        <li><a style="font-family:Comfortaa">partnership</a></li>                    
+                    </ul>
+                </div>
+                <div class="search">
+                    <form role="form">
+                        <i class="fa fa-search"></i>
+                        <div class="field-toggle">
+                            <input type="text" class="search-form" autocomplete="on" placeholder="Search">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </header>
+    <!--/#header-->
+
+    <section id="home-slider">
+        <div class="container">
+            <div class="row">
+                <div class="main-slider">
+                    <div class="slide-text">
+                        <h1>About Us</h1>
+                        <p style="font-size:120%">Indonesian Youth for SDGs is an organization that aims to increase the awareness and involvement of Indonesian Youth for achieving Sustainable Development Goals (SDGs) with stakeholders in Indonesia..</p>
+                        
+                    </div>
+                    <!--img src="images/slider/logo-sgd-2.png" width="30%" class="slider-hill" alt="slider image">-->
+                    <img src="images/slider/logo-sgd-2.png" width="30%" class="slider-house" alt="slider image">
+                    <img src="images/home/slider/birds1.png" class="slider-birds1" alt="slider image">
+                    <img src="images/home/slider/birds2.png" class="slider-birds2" alt="slider image">
+                </div>
+            </div>
+        </div>
+        <div class="preloader"><i class="fa fa-sun-o fa-spin"></i></div>
+    </section>
+    <!--/#home-slider-->
+
+    <section>
+        <div class="container" style="margin-top:5%">
+            
+        </div>
+    </section>
+
+    <section id="action" class="responsive">
+        <div class="vertical-center">
+             <div class="container">
+                <div class="row">
+                    <div class="action take-tour">
+                        <div class="col-sm-7 wow fadeInLeft" data-wow-duration="500ms" data-wow-delay="300ms">
+                            <h1 class="title">What is SDG's??</h1>
+                            
+                        </div>
+                        <div class="col-sm-5 text-center wow fadeInRight" data-wow-duration="500ms" data-wow-delay="300ms">
+                            <div class="tour-button">
+                                <a href="sgd.html" class="btn btn-common">Check This Out</a>
+                             </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+   </section>
+    <!--/#action-->
+
+    
+
+    
+
+    <footer id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12 text-center bottom-separator">
+                    <img src="images/home/under.png" class="img-responsive inline" alt="">
+                </div>
+                <div id="testimonial-container">
+                <div class="row">
+                    <div class="col-md-6">
+                        <h2 class="page-header">Follow Us</h2>
+                        <div class="testimonial">
+                            <div class="col-md-2">
+                                <div class="pull-left">
+                                    <a href="https://twitter.com/indoyouth4sdgs"><img src="images/home/twitico.png" alt=""></a>
+                                </div>
+                                
+                            </div>
+                            <div class="col-md-2">
+                                <div class="pull-left">
+                                    <a href="https://www.facebook.com/indoyouth4sdgs/"><img src="images/home/faceico.png" alt=""></a>
+                                </div>
+                                
+                            </div> 
+                            <div class="col-md-2">
+                                <div class="pull-left">
+                                    <a href="https://www.instagram.com/indoyouth4sdgs/  "><img src="images/home/instaico.png" alt=""></a>
+                                </div>
+                                
+                            </div>  
+                        </div>
+                    </div>
+
+                    <div class="col-md-6">
+                        <h2 class="page-header">Contact Us</h2>
+                           <div class="media">
+                                <div class="pull-left">
+                                    <a><img src="images/home/gmail.png" alt=""></a>
+                                </div>
+                                <div class="media-body" style="margin-top:3%">
+                                    <blockquote>indonesianyouthforsdgs@gmail.com</blockquote>
+                                </div>
+                            </div>
+                            
+                    </div>
+                </div>
+            </div><!--/#testimonial-container-->
+                <div class="col-sm-12">
+                    <div class="copyright-text text-center">
+                        <p>&copy; Bing2Studio 2016. All Rights Reserved.</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!--/#footer-->
+
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/lightbox.min.js"></script>
+    <script type="text/javascript" src="js/wow.min.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>   
+</body>
+</html>
